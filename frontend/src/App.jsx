8339c3550306fcbb3cdb3eddb01d5f64e9fe3c02
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-05-23 00:47:41 -0300

    1er header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
+
 
 const App = () => {
 
@@ -8,6 +13,12 @@ const App = () => {
     <div className='app'>
       <Navbar />
 
+      <Routes>
+      < Route path='/' element={<Home />} />
+        < Route path='/cart' element={<Cart />} />
+        < Route path='/order' element={<PlaceOrder />} />
+
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => { 
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis temporibus rem deleniti est veniam at nihil quos iusto vel! Nulla esse earum aut, quidem quisquam blanditiis nisi hic est.</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
