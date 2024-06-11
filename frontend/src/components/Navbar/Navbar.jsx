9394ac814d2ffa717295e9902676309bedc659bb
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-06-10 21:57:01 -0300

    login-popup creacion

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 
-const Navbar = () => {
+const Navbar = ({ setSwhowLogin }) => {
   const [menu, setMenu] = useState('home');
 
   return (
@@ -22,7 +22,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign in</button>
+        <button onClick={() => setSwhowLogin(true)}>Sign in</button>
       </div>
     </div>
   );
