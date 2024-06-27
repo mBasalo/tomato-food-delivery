76fcd69b1087ab23d421d461d918249e97f07944
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-06-27 20:01:14 -0300

    navbar, header, explore menu and footer responsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favorite food here</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis temporibus rem deleniti est veniam at nihil quos iusto vel! Nulla esse earum aut, quidem quisquam blanditiis nisi hic est.</p>
+            <p className='header-p'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis temporibus rem deleniti est veniam at nihil quos iusto vel! Nulla esse earum aut, quidem quisquam blanditiis nisi hic est.</p>
             <button>View Menu</button>
         </div>
 
