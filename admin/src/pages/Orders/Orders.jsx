6755054f77638ce013745616ba59938c263ce485
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-10-13 15:44:52 -0300

    1.3 Display orders in ADM panel

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './Orders.css'
 import { toast } from 'react-toastify'
 import axios from 'axios'
+import {assets} from "../../assets/assets"
 
 const Orders = ({url}) => {
 
@@ -25,7 +26,45 @@ const Orders = ({url}) => {
 
 
   return (
-    <div></div>
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item, index) => {
+                  if (index == order.items.length -1) {
+                    return item.name + " X " + item.quantity
+                  }
+                  else {
+                    return item.name + " X " + item.quantity + ", "
+                  }
+
+                })}
+              </p>
+
+              <p className="order-item-name">{order.address.firstName + " " + order.address.lastName}</p>
+              
+              <div className="order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>{order.address.city + ", " + order.address.state + 
+                "," + order.address.country + ", " + order.address.zipcode}</p>
+              </div>
+
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+
+            <p>Items : {order.items.length}</p>
+            <p>${order.amount}</p>
+
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
