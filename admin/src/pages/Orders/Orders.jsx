a609e1a1ccd546e187a1f86736d2092b50816adf
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-10-13 15:24:38 -0300

    1.2 Display orders in ADM panel

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Orders.css'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [])
+
+
   return (
-    <div>Orders</div>
+    <div></div>
   )
 }
 
